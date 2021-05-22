e92f0e120f58e035ed3c0a7c98b92361c372035e
Evan Phillips <evan@dhcp-206-153.student-wlan.bowdoin.edu>
2021-05-22 14:30:37 -0400

    updates

--- Main.java
+++ Main.java
@@ -12,7 +12,8 @@ import java.util.*;
  */
 
 public class Main {
-    public static ArrayList<Variable[]> cnf;
+    public static ArrayList<ArrayList<Variable>> cnf;
+    public static ArrayList<ArrayList<Variable>> orig_cnf;
     public static int num_of_variables;
     public static ArrayList<Variable> var_list; // list of variables in cnf
 
@@ -21,7 +22,8 @@ public class Main {
         try {
             File myObj = new File(file);
             Scanner myReader = new Scanner(myObj);
-            cnf = new ArrayList<Variable[]>();
+            cnf = new ArrayList<ArrayList<Variable>>();
+            orig_cnf = new ArrayList<ArrayList<Variable>>();
             while (myReader.hasNextLine()) {
                 String line = myReader.nextLine();
                 if (line.contains("p")) {
@@ -29,11 +31,12 @@ public class Main {
                     Main.num_of_variables = Integer.parseInt(info[2]);
                 } else if (line.charAt(line.length() - 1) == '0') {
                     String literals_str[] = line.split(" ");
-                    Variable clause[] = new Variable[literals_str.length - 1]; // don't include 0 as variable
+                    ArrayList<Variable> clause = new ArrayList<Variable>();
                     for (int i = 0; i < literals_str.length - 1; i++) {
-                        clause[i] = new Variable(Integer.parseInt(literals_str[i]), "null"); // intialize each literal
+                        clause.add(new Variable(Integer.parseInt(literals_str[i]), "null")); // intialize each literal
                     }
                     cnf.add(clause);
+                    orig_cnf.add(clause);
                 }
             }
             myReader.close();
@@ -46,27 +49,29 @@ public class Main {
         for (int i = 1; i < num_of_variables + 1; i++) {
             var_list.add(new Variable(i, "null"));
         }
-
         return 0;
     }
 
     /**
-     * checks if the cnf is consistent: at least one literal evaluates to true in
-     * each clause
+     * checks if the cnf is consistent
      */
     public static boolean isConsistent() {
-        for (Variable[] clause : cnf) {
-            boolean isSat = false;
+        if (containsEmpty()) { // edge case: if the last clause is empty
+            return false;
+        }
+        for (ArrayList<Variable> clause : cnf) {
             for (Variable lit : clause) {
-                if (lit.VariableAssign.equals("true")) {
-                    isSat = true;
+                for (ArrayList<Variable> clause2 : cnf) {
+                    for (Variable lit2 : clause2) {
+                        if (lit.value == lit2.value * -1) {
+                            return false;
+                        }
+                    }
                 }
-            }
-            if (!isSat) {
-                return false;
             }
         }
         return true;
+
     }
 
     /**
@@ -74,7 +79,7 @@ public class Main {
      * literals that evaluate to false
      */
     public static boolean containsEmpty() {
-        for (Variable[] clause : cnf) {
+        for (ArrayList<Variable> clause : cnf) {
             boolean isEmpty = true;
             for (Variable lit : clause) {
                 if (!(lit.VariableAssign.equals("false"))) {
@@ -86,7 +91,6 @@ public class Main {
             }
         }
         return false;
-
     }
 
     /**
@@ -94,8 +98,8 @@ public class Main {
      * 
      * @param literal the variable which we set to True
      */
-    public static ArrayList<Variable[]> setVarTrue(Variable literal) {
-        for (Variable[] clause : cnf) {
+    public static ArrayList<ArrayList<Variable>> setVarTrue(Variable literal) {
+        for (ArrayList<Variable> clause : cnf) {
             for (Variable lit : clause) {
                 if (lit.value == literal.value) {
                     lit.VariableAssign = "true";
@@ -113,8 +117,8 @@ public class Main {
      * 
      * @param literal the literal which we set to False
      */
-    public static ArrayList<Variable[]> setVarFalse(Variable literal) {
-        for (Variable[] clause : cnf) {
+    public static ArrayList<ArrayList<Variable>> setVarFalse(Variable literal) {
+        for (ArrayList<Variable> clause : cnf) {
             for (Variable lit : clause) {
                 if (lit.value == literal.value) {
                     lit.VariableAssign = "false";
@@ -128,24 +132,32 @@ public class Main {
 
     }
 
+    public static void removeClause(Variable literal) {
+        for (int i = 0; i < cnf.size(); i++) {
+            for (Variable lit : cnf.get(i)) {
+                if (lit.value == literal.value) {
+                    cnf.remove(cnf.get(i));
+                    i--;
+                }
+            }
+        }
+
+    }
+
     /**
      * if a literal is a unit clause, appliies unit propagation: assigns the
      * necessary value to the literal in order to make the literal true
      * 
      * @param literal the variable which we attempt to apply unit propogation
      */
-    public static boolean unitProp(Variable literal) {
-        for (Variable[] clause : cnf) {
-            if (clause.length == 1 && clause[0].value == literal.value) {
-                setVarTrue(literal);
-                return true;
-            }
-            if (clause.length == 1 && clause[0].value * -1 == literal.value) {
-                setVarFalse(literal);
-                return true;
+    public static void unitProp() {
+        for (int i = 0; i < cnf.size(); i++) {
+            if (cnf.get(i).size() == 1 && cnf.get(i).get(0).VariableAssign.equals("null")) {
+                setVarTrue(cnf.get(i).get(0)); // discards all complements
+                removeClause(cnf.get(i).get(0));
+                i--;
             }
         }
-        return false;
     }
 
     /**
@@ -154,50 +166,71 @@ public class Main {
      * 
      * @param literal the literal which we attempt to apply pure literal elimination
      */
-    public static boolean pureLiteral(Variable literal) {
-        boolean isPositive = false;
-        boolean isNegative = false;
-        for (Variable[] clause : cnf) {
-            for (Variable lit : clause) {
-                // check to see if lit is all positive or all negative
-                if (literal.value == lit.value) { // pos case
-                    isPositive = true;
+    public static void pureLiteral() {
+        for (int i = 0; i < cnf.size(); i++) {
+            for (Variable lit : cnf.get(i)) {
+                boolean isNotPure = false;
+                for (int j = 0; j < cnf.size(); j++) {
+                    for (Variable lit2 : cnf.get(j)) {
+                        if (lit.value == lit2.value * -1) {
+                            isNotPure = true;
+                        }
+                    }
                 }
-                if (literal.value == lit.value * -1) { // neg case
-                    isNegative = true;
+                if (!isNotPure) {
+                    System.out.println(cnf.size());
+                    removeClause(lit);
                 }
 
             }
         }
-
-        if (isPositive && !isNegative) {
-            setVarTrue(literal);
-            return true;
-        }
-        if (!isPositive && isNegative) {
-            setVarFalse(literal);
-            return true;
-        }
-        return false;
     }
 
-    public static boolean DPLL(ArrayList<Variable[]> cnf) {
+    public static ArrayList<ArrayList<Variable>> addUnit(Variable var) {
+        ArrayList<Variable> unit = new ArrayList<Variable>();
+        unit.add(var);
+        cnf.add(unit);
+        return cnf;
+    }
+
+    public static void resetVarList() {
+        for (int i = 1; i < num_of_variables; i++) {
+            var_list.add(new Variable(i * -1, "null")); // i * -1 ??
+        }
+        cnf.clear();
+        for (ArrayList<Variable> clause : orig_cnf) {
+            cnf.add(clause);
+        }
+    }
+
+    public static boolean DPLL(ArrayList<ArrayList<Variable>> cnf) {
+        System.out.println(cnf.size());
+        // for (ArrayList<Variable> clause : cnf) {
+        // for (Variable lit : clause) {
+        // System.out.println(lit.value);
+        // }
+        // System.out.println("\n");
+        // }
+        // System.out.println("––––––––––––––––––––");
+
         if (isConsistent()) {
             return true;
         }
         if (containsEmpty()) {
             return false;
         }
-        Variable var = var_list.remove(0); // retrieves literals in sequential order
-        System.out.println(var.value);
-        // if (unitProp(var) || pureLiteral(var)) {
-        // return DPLL(cnf);
-        // } else {
-        // return DPLL(setVarTrue(var)) || DPLL(setVarFalse(var));
-        // }
+        unitProp();
+        // pureLiteral();
 
-        return DPLL(setVarTrue(var)) || DPLL(setVarFalse(var));
+        if (var_list.size() == 0) { // all vars have been checked
+            resetVarList();
+            System.out.println(cnf.size());
+        }
 
+        Variable var = var_list.remove(0);
+        // System.out.println(var.value);
+        Variable negVar = new Variable(var.value * -1, "null");
+        return DPLL(addUnit(var)) || DPLL(addUnit(negVar));
     }
 
     public static void main(String[] args) {
@@ -207,10 +240,6 @@ public class Main {
         } else {
             System.out.println("FALSE");
         }
-        // for (Variable[] clause : cnf) {
-        // for (Variable v : clause) {
-        // System.out.println(v.VariableAssign);
-        // }
-        // }
+
     }
 }
